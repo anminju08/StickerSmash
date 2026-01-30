6fe89fa94d6a7ccf2045aee7ad28c06b9d0c6453
안민주 <anminju0310@gmail.com>
2026-01-30 17:13:08 +0900

    feat :: 홈화면 이미지 추가 및 스타일링

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
 import { Text, View,  StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
+import { Image } from 'expo-image';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-      Go to About Screen</Link>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
     </View>
   );
 }
@@ -25,5 +28,13 @@ const styles = StyleSheet.create({
     fontSize:20,
     textDecorationLine:'underline',
     color:'white',
-  }
+  },
+  imageContainer:{
+    flex:1,
+  },
+  image:{
+    width:320,
+    height:440,
+    borderRadius:18,
+  },
 });
