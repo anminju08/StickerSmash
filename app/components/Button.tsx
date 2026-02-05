8b63ddf7163ff6488860082efae22c64cea4ca45
안민주 <anminju0310@gmail.com>
2026-02-05 23:31:40 +0900

    feat :: button 기능

---
+++ app/components/Button.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+
+type Props = {
+    label:string;
+};
+export default function Button({ label }: Props) {
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
