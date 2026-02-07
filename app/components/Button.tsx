41633ea4c3c83372da21d2387f7a2979a5ecdd9c
안민주 <anminju0310@gmail.com>
2026-02-07 22:30:47 +0900

    feat :: button style 수정

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -12,3 +12,26 @@ export default function Button({ label }: Props) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  buttonLabel: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
