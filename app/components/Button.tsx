0a5c12475e0c5f2233883e02e61ceadfab1e75d9
안민주 <anminju0310@gmail.com>
2026-02-27 17:09:07 +0900

    feat :: button 함수 추가

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,6 +7,20 @@ type Props = {
 };
 
 export default function Button({ label, theme }: Props) {
+  if(theme === 'primary'){
+    return(
+        <View style={[styles.buttonContainer,
+            {borderWidth:4, borderColor:'#ffd33d', borderRadius:18 },
+        ]}>
+            <Pressable style={[styles.button, {backgroundColor:'#fff'}]}
+            onPress={() => alert('You pressed a button!')}>
+                <FontAwesome name='picture-o' size={18} color="#25292e" style={styles.buttonIcon} />
+                <Text style={[styles.buttonLabel, {color:'#25292e'}]}>{label}</Text>
+            </Pressable>
+        </View>
+    );
+  }
+
   return (
     <View style={styles.buttonContainer}>
       <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
